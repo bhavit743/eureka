220da4178e20341b1cd1248fbd06942da9760172
Yash Saraogi <yashsaraogi67@gmail.com>
2021-08-30 00:33:33 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Incentives  from "./components/Incentives";
 import './App.css'
 import { useState } from "react";
@@ -6,6 +5,15 @@ import Tracks from "./components/Tracks";
 import Zonals from "./components/Zonals";
 import Competition from "./components/Competition";
 
+import logo from './Cave-min (2).jpg';
+
+import Header from './components/Header/index';
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+import Home from './pages';
+import Page1 from './pages/page1';
+import Page2 from './pages/page2';
+import Page3 from './pages/page3';
+import Page4 from './pages/page4';
 const App = () => {
   const [tracks,settracks] = useState([
 
@@ -40,11 +48,22 @@ const App = () => {
 
   ])
   return (
+    <Router>
+    <Header />
     <div>
     {/* <Tracks tracks={tracks} /> */}
     {/* <Zonals zones={zones} /> */}
-  <Competition />
+  {/* <Competition /> */}
     </div>
+   
+   <Switch>
+     <Route path='/' exact component={Home} />
+     <Route path='/page1' exact component={Page1} />  
+     <Route path='/page2' exact component={Page2} />
+     <Route path='/page3' exact component={Page3} />  
+     <Route path='/page4' exact component={Page4} />    
+   </Switch>
+   </Router>
   )
 }
 
@@ -57,32 +76,8 @@ export default App
 
 
 
-=======
-import logo from './Cave-min (2).jpg';
-import './App.css';
-import Header from './components/Header/index';
-import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
-import Home from './pages';
-import Page1 from './pages/page1';
-import Page2 from './pages/page2';
-import Page3 from './pages/page3';
-import Page4 from './pages/page4';
 
-function App() {
-  return (
-    <Router>
-      <Header />
-    <Switch>
-      <Route path='/' exact component={Home} />
-      <Route path='/page1' exact component={Page1} />  
-      <Route path='/page2' exact component={Page2} />
-      <Route path='/page3' exact component={Page3} />  
-      <Route path='/page4' exact component={Page4} />    
-    </Switch>
-    </Router>
-    
-  );
-}
 
-export default App;
->>>>>>> 3e4e39781f27f5587a17b6b7a41dafe5b669378e
+
+
+
